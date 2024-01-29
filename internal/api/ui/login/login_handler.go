a19f5b462b1e0a67872abeead8f0e30b1b7a19f8
Anthony Lawn <thepaperpilot@gmail.com>
2024-01-28 18:48:55 -0600

    Added change username button back

--- internal/api/ui/login/login_handler.go
+++ internal/api/ui/login/login_handler.go
@@ -83,7 +83,6 @@ func (l *Login) handleLoginNameCheck(w http.ResponseWriter, r *http.Request) {
 	}
 	userAgentID, _ := http_mw.UserAgentIDFromCtx(r.Context())
 	loginName := data.LoginName
-	logging.WithFields("loginName", loginName).Info("!!")
 	if !strings.Contains(loginName, "@") {
 		loginName = loginName + "@incremental.social"
 	}
